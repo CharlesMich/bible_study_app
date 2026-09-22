4196a451c1ab684d933c3f7ff2431f15b1b28af1
CharlesMich <charleedongo@hotmail.com>
2026-09-22 12:38:34 -0500

    data

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -44,6 +44,11 @@ function Home() {
       type: "external",
       url: "https://www.amazon.com/stores/Charles-Michael/author/B07SMB94PD?ref=ap_rdr&shoppingPortalEnabled=true&ccs_id=2c7436cb-fa0b-4d4d-a9c1-85b5152d75c6",
     },
+    {
+      name: "Call on HIM - Youtube",
+      type: "external",
+      url: "https://www.youtube.com/@Callonhim",
+    },
   ];
 
   function handleClick(item) {
